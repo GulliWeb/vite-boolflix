702e06d8506c8fe5465b4419e647f9a79b1c3f54
Andrea Gulli <andreagulli11@gmail.com>
2024-09-23 17:05:20 +0200

    complete MILESTONE 1

--- src/components/AppSearchBar.vue
+++ src/components/AppSearchBar.vue
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default {
   data() {
     return {
+        films:[],
         // indirizzo API searchMovies
         apiAdress: 'https://api.themoviedb.org/3/search/movie',
         // Chiave API
@@ -17,10 +18,17 @@ export default {
   methods:{
     // Metodo per effettuare una chiamata API per ricavare i vari film con le relative informazioni
     apiCall(apiKey, apiQuery){
+        // Controllo che verifica se l'input è vuota
+        if (!this.inputSearch) {
+            alert(`Attenzione! campo di ricerca vuoto.`)
+            txtSearch.focus()
+            return
+        }
         const apiUrl = `${this.apiAdress}?api_key=${this.apiKey}&query=${this.inputSearch}`
         axios.get(apiUrl)
-        .then(function (response) {
-            console.log(response);
+        .then((response) => {
+            console.log(response.data.results);
+            this.films = response.data.results
           })
           .catch(function (error) {
             console.log(error);
@@ -35,10 +43,14 @@ export default {
 
 <template>
     <div class="input">
-        <input type="text" id="txtSearch" v-model="inputSearch">
-        <button id="btnSearch" class="btn btn-primary" @click="apiCall()">Search</button>
-
+        <input type="text" id="txtSearch" v-model="inputSearch" @keyup.enter="apiCall">
+        <button id="btnSearch" class="btn btn-primary" @click="apiCall">Search</button>
     </div>
+    <ul>
+        <li v-for="film in films" :key="film.id">
+            <h3>{{ film.title }}</h3>
+        </li>
+    </ul>
 </template>
 
 <style lang="scss" scoped>
